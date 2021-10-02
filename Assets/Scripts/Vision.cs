680b59621529b1d9e0ed949d32fab3c71df85fbf
Marwan Zaky <64248203+marwanzaky@users.noreply.github.com>
2021-10-02 15:04:09 +0100

    Mesh generation rotating along with the parent game object rotation

--- Assets/Scripts/Vision.cs
+++ Assets/Scripts/Vision.cs
@@ -8,7 +8,7 @@ public class Vision : MonoBehaviour
     const float START_DEGRESS = 90f;
     float lastLength = 0;
 
-    [SerializeField, Range(0f, 100f)] int length = 12;
+    [SerializeField, Range(1, 100)] int length = 12;
     [SerializeField, Range(0f, 360f)] float range = 90f;
     [SerializeField, Range(.01f, 100f)] float maxDistance = 10f;
     [SerializeField] bool debug = false;
@@ -33,24 +33,33 @@ public class Vision : MonoBehaviour
 
         if (length > 0)
             Generate();
+
+        foreach (var el in meshes)
+        {
+            el.gameObject.transform.rotation = Quaternion.identity;
+            el.gameObject.transform.position = Vector3.zero;
+        }
     }
 
     void Generate()
     {
         for (int i = 0; i < length; i++)
         {
-            Vector3 startPos = Vector3.zero;
-            Vector3 point1 = Raycast(i).point;
-            Vector3 point2 = Raycast(i + 1).point;
+            Vector3[] triangleVers = new Vector3[3] {
+                transform.position,
+                Raycast(i).worldSpacePoint,
+                Raycast(i + 1).worldSpacePoint
+            };
 
-            DrawTriangle(i, startPos, point1, point2);
+            Mesh trianglMesh = MeshGeneration.Triangle(triangleVers[0], triangleVers[1], triangleVers[2]);
+            meshes[i].meshFilter.mesh = trianglMesh;
         }
     }
 
-    (RaycastHit hit, Vector3 point) Raycast(int i)
+    (RaycastHit hit, Vector3 worldSpacePoint) Raycast(int i)
     {
         float degress = range / length * i;
-        float offsetDegress = range / 2f;
+        float offsetDegress = range / 2f + transform.eulerAngles.y;
 
         Vector3 ori = transform.position;
         Vector3 dir = MathfX.DegressToVector3D(degress - offsetDegress + START_DEGRESS);
@@ -63,12 +72,8 @@ public class Vision : MonoBehaviour
             Debug.Log("Can see target");
 
         return (hit, hit.collider ?
-                    Vector3X.IgnoreY(hit.point) : dir * maxDistance);
-    }
-
-    void DrawTriangle(int i, Vector3 startPos, Vector3 endPos, Vector3 nextEndPos)
-    {
-        meshes[i].meshFilter.mesh = MeshGeneration.Triangle(startPos, endPos, nextEndPos);
+                    Vector3X.IgnoreY(hit.point, transform.position.y) :
+                    transform.position + dir * maxDistance);
     }
 
     void SpawnVisionMesh()
