8a03c518e55348c9f26aa8f3773c7fc4a3dcc595
Marwan Zaky <64248203+marwanzaky@users.noreply.github.com>
2021-10-03 23:15:43 +0100

    OnSeeingTarget() callback added, .gitignore updated

--- Assets/Enemy Vision/Scripts/Vision.cs
+++ Assets/Enemy Vision/Scripts/Vision.cs
@@ -6,9 +6,12 @@ public class Vision : MonoBehaviour
     List<VisionMesh> meshes = new List<VisionMesh>();
 
     const float START_DEGRESS = 90f;
+
     float lastLength = 0;
 
-    [SerializeField, Range(1, 100)] int length = 12;
+    public bool seeingTarget = false;
+
+    [SerializeField, Range(1, 120)] int length = 12;
     [SerializeField, Range(0f, 360f)] float range = 90f;
     [SerializeField, Range(.01f, 100f)] float maxDistance = 10f;
     [SerializeField] bool debug = false;
@@ -34,6 +37,9 @@ public class Vision : MonoBehaviour
         if (length > 0)
             Generate();
 
+        if (seeingTarget)
+            OnSeeingTarget();
+
         foreach (var el in meshes)
         {
             el.gameObject.transform.rotation = Quaternion.identity;
@@ -43,20 +49,28 @@ public class Vision : MonoBehaviour
 
     void Generate()
     {
+        seeingTarget = false;
+
         for (int i = 0; i < length; i++)
         {
+            var hit1 = Raycast(i);
+            var hit2 = Raycast(i + 1);
+
             Vector3[] triangleVers = new Vector3[3] {
                 transform.position,
-                Raycast(i).worldSpacePoint,
-                Raycast(i + 1).worldSpacePoint
+                hit1.worldSpacePoint,
+                hit2.worldSpacePoint
             };
+
+            if (!seeingTarget)
+                seeingTarget = hit1.seeingTarget;
 
             Mesh trianglMesh = MeshGeneration.Triangle(triangleVers[0], triangleVers[1], triangleVers[2]);
             meshes[i].meshFilter.mesh = trianglMesh;
         }
     }
 
-    (RaycastHit hit, Vector3 worldSpacePoint) Raycast(int i)
+    (bool seeingTarget, Vector3 worldSpacePoint) Raycast(int i)
     {
         float degress = range / length * i;
         float offsetDegress = range / 2f + transform.eulerAngles.y;
@@ -66,14 +80,12 @@ public class Vision : MonoBehaviour
 
         RaycastHit hit = RaycastHitX.Cast(ori, dir, layerMask, maxDistance, debug);
 
-        if (debug &&
-            hit.collider != null &&
-            hit.collider.gameObject.layer == LayerMask.NameToLayer(targetLayer))
-            Debug.Log("Can see target");
+        return (hit.collider &&
+                hit.collider.gameObject.layer == LayerMask.NameToLayer(targetLayer),
 
-        return (hit, hit.collider ?
-                    Vector3X.IgnoreY(hit.point, transform.position.y) :
-                    transform.position + dir * maxDistance);
+                hit.collider ?
+                Vector3X.IgnoreY(hit.point, transform.position.y) :
+                transform.position + dir * maxDistance);
     }
 
     void SpawnVisionMesh()
@@ -95,5 +107,10 @@ public class Vision : MonoBehaviour
 
         if (debug)
             Debug.Log("Clear and re-spawn the vision mesh");
+    }
+
+    protected virtual void OnSeeingTarget()
+    {
+
     }
 }
